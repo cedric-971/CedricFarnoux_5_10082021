491c6fabd52f063e7120e83566e7e46bf63ee163
Cedric Farnoux <djibrilone@hotmail.com>
2021-08-22 00:52:30 +0200

    modification page produit

--- product.js
+++ product.js
@@ -7,9 +7,13 @@ console.log(id)
 const products2 = document.getElementById("products2");
 console.log(products2)
 
-fetch("http://localhost:3000/api/teddies/"+ id)
-.then ((res) => res.json())
-.then ((data) => (
+ 
+
+
+
+  fetch ("http://localhost:3000/api/teddies/"+ id)
+    .then ((res) => res.json())
+    .then ((data) =>  {
     products2.innerHTML = 
     
     `
@@ -21,16 +25,30 @@ fetch("http://localhost:3000/api/teddies/"+ id)
         <p>  
             ${data.description}
         </p>
-        <p>${data.price}  </p>
-   
-    <p>Couleur : </p><input type = "text">
-    <p>Quantité : </p><input type = "text"></br>
-    <button>Ajouter au panier</button>
+        <h6>Prix :</h6>
+        <p>${data.price /100}€ </p>
+    <form>
+    <label for="quantity">Quantité :</label>
+        <input type="number" name="quantity" id="quantity"></br>
+    
+    <label for= "color">Couleur :</label>
+        <select name="color" id="color">
+            <option value="">--Couleur--</option>
+            <option value="${data.colors[0]}">${data.colors[0]}</option>
+            <option value="${data.colors[1]}">${data.colors[1]}</option>
+            <option value="${data.colors[2]}">${data.colors[2]}</option>
+            <option value="${data.colors[3]}">${data.colors[3]}</option>
+        </select>
+    </form>
+    <button onclick="setData()">Ajouter au panier</button>
     </div>
     
 </div>
     `
-)
-
+    const form = document.getElementById("quantity");
+    console.log(form); 
+ }
 );
+
+
 
